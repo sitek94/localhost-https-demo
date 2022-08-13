f04f2dad50dd7e764beb0ddb02f97a49e4ad1ac6
Maciek <msitkowski94@gmail.com>
2022-08-13 16:45:32 +0200

    create locally signed SSL certs with mkcert to make https server work

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import https from 'node:https'
+import fs from 'node:fs'
+
+const options = {
+  key: fs.readFileSync('certs/localhost-key.pem'),
+  cert: fs.readFileSync('certs/localhost.pem'),
+}
 
 https
-  .createServer((req, res) => {
+  .createServer(options, (req, res) => {
     res.writeHead(200)
     res.end('hello world\n')
   })
